7110184a908992b321a036bba6b81f979374934d
issuser <issuser@ISS-CR2921.stf.nus.edu.sg>
2018-03-16 14:58:23 +0800

    Menu prompt

--- src/iis/TodoList.java
+++ src/iis/TodoList.java
@@ -2,6 +2,7 @@ package iis;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class TodoList {
@@ -28,12 +29,26 @@ public class TodoList {
 		return new HashSet<TodoItem>(list);
 	}
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		System.out.println("1. List items");
-		System.out.println("2. Add item");
-		System.out.println("3. Delete item");
-		System.out.println("4. Edit item");
-		System.out.println("5. Mark item");
+		Scanner reader = new Scanner(System.in); // Reading from System.in
+		int choice = -1;
+		do {
+			System.out.println("*************************************");
+			System.out.println("");
+			System.out.println("1. List items");
+			System.out.println("2. Add item");
+			System.out.println("3. Delete item");
+			System.out.println("4. Edit item");
+			System.out.println("5. Mark item");
+			System.out.println("0. Exit");
+			System.out.println("");
+			System.out.print("Enter you choice > ");
+			
+			choice = reader.nextInt();
+		} while (choice != 0);
+
+		System.out.print("Exited :)");
 	}
 
 }
